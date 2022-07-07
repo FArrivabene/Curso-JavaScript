f8ab5643dd7df01e48990ff7c69b57965c67acff
Fabio Arriabene <arrivabene.fabio@outlook.com>
2022-07-07 23:08:48 +0100

    New

---
+++ Aula6/index.js
@@ -0,0 +1,20 @@
+let nome = "Fabio"; //String
+
+console.log(nome, "nasceu em 1995.");
+console.log("Em 2000", nome, "conheceu Amanda.");
+console.log(nome, "casou com Amanda em 2020.");
+console.log(nome, "e Amanda tem um doguinho.");
+console.log("O cachorro do", nome, "se chama tequila.");
+
+let test1; //Declarou a variável
+test1 = "Fabio";
+console.log(test1, "gosta do tobias");
+test1 = "Otavio";
+console.log(test1);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Utilizamos por convenção camelCase
+// Case-sensitives
+// Não podemos redeclarar variáveis com Let
+// Não utilize VAR, utilize Let
